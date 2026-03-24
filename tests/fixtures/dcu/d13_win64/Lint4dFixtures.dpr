ca47b3fa99c1b51bd858781768200b7071f3786a
Antoine Gaudreau Simard <a.simard@multidev.net>
2026-03-24 11:30:42 -0400

    chore: add Delphi project scaffolding for DCU fixtures

---
+++ tests/fixtures/dcu/d13_win64/Lint4dFixtures.dpr
@@ -0,0 +1,13 @@
+program Lint4dFixtures;
+
+uses
+  Lint4dFixture.Classes in 'Lint4dFixture.Classes.pas',
+  Lint4dFixture.Interfaces in 'Lint4dFixture.Interfaces.pas',
+  Lint4dFixture.Records in 'Lint4dFixture.Records.pas',
+  Lint4dFixture.Enums in 'Lint4dFixture.Enums.pas',
+  Lint4dFixture.Generics in 'Lint4dFixture.Generics.pas',
+  Lint4dFixture.Inheritance in 'Lint4dFixture.Inheritance.pas',
+  Lint4dFixture.Torture in 'Lint4dFixture.Torture.pas';
+
+begin
+end.

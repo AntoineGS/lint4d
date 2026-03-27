8aceef01baa4545a7b3f4600ba13deb697aeff72
Antoine Gaudreau Simard <a.simard@multidev.net>
2026-03-27 16:11:39 -0400

    test: add initial format test fixtures

---
+++ tests/fixtures/format/indent/basic_expected.pas
@@ -0,0 +1,26 @@
+unit BasicIndent;
+
+interface
+
+type
+  TMyClass = class
+  private
+    FValue: Integer;
+  public
+    procedure DoSomething;
+  end;
+
+implementation
+
+procedure TMyClass.DoSomething;
+var
+  x: Integer;
+begin
+  x := 1;
+  if x > 0 then
+  begin
+    x := x + 1;
+  end;
+end;
+
+end.
